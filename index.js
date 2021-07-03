2d77d6de853b02782527d59379d64afce5239996
ravisaikiran <720818106027@hit.edu.in>
2021-07-03 19:42:57 +0530

    save edit changes to local storage

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const geneateNewCard = (taskData) => `
       <a href="#" class="btn btn-primary">${taskData.taskType}</a>
     </div>
     <div class="card-footer">
-        <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+        <button type="button" class="btn btn-outline-primary float-end" id=${taskData.id}>Open Task</button>
     </div>
   </div>
 </div>
@@ -29,6 +29,9 @@ const loadInitialCardData= () => {
     taskContainer.insertAdjacentHTML("beforeend",geneateNewCard(cardObject));
     globalStore.push(cardObject);
   } )
+};
+const updateLocalStorage=()=>{
+  localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
 };
 const saveChanges = () =>{
     const taskData = {
@@ -73,5 +76,39 @@ const editCard=(event)=>{
   taskTitle.setAttribute("contenteditable","true");
   taskDescription.setAttribute("contenteditable","true");
   taskType.setAttribute("contenteditable","true");
+  submitButton.setAttribute("onclick","saveEditChanges.apply(this,arguments)");
   submitButton.innerHTML="Save Changes";
+};
+const saveEditChanges=(event)=>{
+  event=window.event;
+  const targetID=event.target.id;
+  const tagname=event.target.tagName;
+  let parentElement;
+  if(tagname==="BUTTON"){
+    parentElement=event.target.parentNode.parentNode;
+  }else{
+    parentElement=event.target.parentNode.parentNode.parentNode;
+  }
+  let taskTitle=parentElement.childNodes[5].childNodes[1];
+  let taskDescription=parentElement.childNodes[5].childNodes[3];
+  let taskType=parentElement.childNodes[5].childNodes[5];
+  let submitButton=parentElement.childNodes[7].childNodes[1];
+  const updatedData={
+    taskTitle:taskTitle.innerHTML,
+    taskType:taskType.innerHTML,
+    taskDescription:taskDescription.innerHTML,
+  };
+  globalStore=globalStore.map((task)=>{
+    if(task.id=targetID){
+      return{
+        id:task.id,
+        imageUrl:task.imageUrl,
+        taskTitle:updatedData.taskTitle,
+        taskType:updatedData.taskType,
+        taskDescription:updatedData.taskDescription,
+      };
+    }
+    return task;
+  });
+  updateLocalStorage();
 };
