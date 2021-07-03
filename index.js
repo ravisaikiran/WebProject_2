2c96f66eafe8788861faf858d575fac386eb0e7d
ravisaikiran <720818106027@hit.edu.in>
2021-07-03 19:15:50 +0530

    delete and edit features

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const taskContainer=document.querySelector(".task__container");
 
-const globalStore = [];
+let globalStore = [];
 
 const geneateNewCard = (taskData) => `
 <div class="col-md-6 col-lg-4" id=${taskData.id}>
 <div class="card">
     <div class="card-header d-flex justify-content-end gap-2">
-        <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
+        <button type="button" class="btn btn-outline-success" id=${taskData.id} onclick="editCard.apply(this, arguments)"><i class="fas fa-pencil-alt" id=${taskData.id} onclick="editCard.apply(this, arguments)"></i></button>
+        <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this, arguments)"><i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this, arguments)"></i></button>
     </div>
     <img src=${taskData.imageUrl} class="card-img-top" alt="...">
     <div class="card-body">
@@ -42,4 +42,36 @@ const saveChanges = () =>{
     globalStore.push(taskData);
 
     localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
+};
+const deleteCard = (event)=>{
+  event=window.event;
+  const targetID=event.target.id;
+  const tagname=event.target.tagName;
+  globalStore=globalStore.filter((cardObject)=> cardObject.id!==targetID);
+  localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
+  if(tagname === "BUTTON"){
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+  }else{
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+  }
+  //taskContainer.removeChild(document.getElementById(targetID));
+};
+const editCard=(event)=>{
+  event=window.event;
+  const targetID=event.target.id;
+  const tagname=event.target.tagName;
+  let parentElement;
+  if(tagname==="BUTTON"){
+    parentElement=event.target.parentNode.parentNode;
+  }else{
+    parentElement=event.target.parentNode.parentNode.parentNode;
+  }
+  let taskTitle=parentElement.childNodes[5].childNodes[1];
+  let taskDescription=parentElement.childNodes[5].childNodes[3];
+  let taskType=parentElement.childNodes[5].childNodes[5];
+  let submitButton=parentElement.childNodes[7].childNodes[1];
+  taskTitle.setAttribute("contenteditable","true");
+  taskDescription.setAttribute("contenteditable","true");
+  taskType.setAttribute("contenteditable","true");
+  submitButton.innerHTML="Save Changes";
 };
